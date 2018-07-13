a141295be1e2056701d51c7969614c0ea75ab0de
kamransumar <kamransumar@gmail.com>
2018-07-13 16:47:50 +0300

    added js and icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,32 @@
-var numToBePonged=[];
+var numToBePonged =[];
 
 function pingPong (number){
-  for (var index = 1; index <= number; index += 1){
-    if(index % 15 === 0){
-    numToBePonged.push("pingpong")
-    }
-    else if(index % 3 === 0){
+  for (var i = 1; i <= number; i+= 1){
+    if(i % 15 === 0){
+    numToBePonged.push("pingpong");
+
+  }else if(i % 3 === 0){
       numToBePonged.push("ping");
     }
-    else if(index % 5 === 0){
+    else if(i % 5 === 0){
       numToBePonged.push("pong");
+   }
+   else{
+     numToBePonged.push(i);
+   };
+};
+return numToBePonged;
+};
 
-  }
-}
+$(document).ready(function(){
+  $("form#willitpong").submit(function(event){
+    event.preventDefault();
+    var number= parseInt($("input#formnumber").val());
+    pingPong(number);
+
+    numToBePonged.forEach(function(number){
+      $("ul#output").append("<li>" + number + "</li>");
+console.log(numToBePonged)
+    });
+  });
+});
