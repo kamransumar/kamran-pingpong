f623b016ccf0475555f82841163b132c2975362a
kamransumar <kamransumar@gmail.com>
2018-07-13 15:13:53 +0300

    added new forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+var numToBePonged=[];
 
+function pingPong (number){
+  for (var index = 1; index <= number; index += 1){
+    if(index % 15 === 0){
+    numToBePonged.push("pingpong")
+    }
+    else if(index % 3 === 0){
+      numToBePonged.push("ping");
+    }
+    else if(index % 5 === 0){
+      numToBePonged.push("pong");
+
+  }
+}
